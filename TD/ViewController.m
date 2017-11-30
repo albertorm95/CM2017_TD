e259c16a79b23ff2065f63d43361a44fca1248fd
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2017-11-29 20:01:14 -0600

    Initial Commit

---
+++ TD/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TD
+//
+//  Created by Walter Gonzalez Domenzain on 29/11/17.
+//  Copyright © 2017 UAG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
